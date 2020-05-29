9fe116ad44314d7b27b54ee6b933bc0fef10bfd0
enetrebko <enetrebko@gmail.com>
2020-05-29 20:55:35 +0300

    fix static

--- hasker/settings.py
+++ hasker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7#_o8oc2od1un53#se+y0n-qj-@^7kzbx7$wm^lyob(v%z=7x*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,10 +133,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-      os.path.join(BASE_DIR, 'static/'),
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+else:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static/'),
     )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

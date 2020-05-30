cc98a940c21a5b4f89ee3e5df9d9e4b35a065054
enetrebko <enetrebko@gmail.com>
2020-05-30 20:43:06 +0300

    create settings folder

---
+++ hasker/settings/local.py
@@ -0,0 +1,28 @@
+from .base import *
+
+DEBUG = True
+
+SECRET_KEY = '7#_o8oc2od1un53#se+y0n-qj-@^7kzbx7$wm^lyob(v%z=7x*'
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hasker_db',
+        'USER': 'postgres',
+        'PASSWORD': '1134',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'hasker.app@mail.ru'
+EMAIL_HOST_PASSWORD = 'otus1134'
---
+++ hasker/settings/prod.py
@@ -0,0 +1,26 @@
+from .base import *
+
+DEBUG = False
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = os.getenv('STATIC_ROOT')
+MEDIA_ROOT = os.getenv('MEDIA_ROOT')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('HASKER_DATABASE_NAME'),
+        'USER': os.getenv('HASKER_DATABASE_USER'),
+        'PASSWORD': os.getenv('HASKER_DATABASE_PASSWORD'),
+        'HOST': os.getenv('HASKER_DATABASE_HOST'),
+        'PORT': os.getenv('HASKER_DATABASE_PORT')
+    }
+}
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
